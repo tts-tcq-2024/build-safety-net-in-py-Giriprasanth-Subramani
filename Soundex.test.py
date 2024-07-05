76b8e508cc1818625d2cbe670ca3750e3ee6d857
Giriprasanth-Subramani <167963812+Giriprasanth-Subramani@users.noreply.github.com>
2024-07-05 21:32:03 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -2,19 +2,12 @@ import unittest
 from Soundex import generate_soundex
 
 class TestSoundex(unittest.TestCase):
-    def test_empty_string(self):
+    def test_cases(self):
         self.assertEqual(generate_soundex(""), "")
-    def test_single_character(self):
         self.assertEqual(generate_soundex("G"), "G000")
-    def test_other_cases(self):
-        self.assertEqual(generate_soundex("Saravanan"), "S615")
-        self.assertEqual(generate_soundex("Rupert"), "R163")
-        self.assertEqual(generate_soundex("Air"), "A600")
         self.assertEqual(generate_soundex("Ashcraft"), "A261")
-        self.assertEqual(generate_soundex("Ashcroft"), "A261")
         self.assertEqual(generate_soundex("Pfister"), "P236")
-        self.assertEqual(generate_soundex("A23F"), "A100")
-        self.assertEqual(generate_soundex("AEIOU"), "A000")
+
 
 if __name__ == '__main__':
  unittest.main()
