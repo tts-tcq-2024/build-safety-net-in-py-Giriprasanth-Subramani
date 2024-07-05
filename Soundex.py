c4f344b7f4deb35ecd04e7448fbc0f30efbcbbfd
Giriprasanth-Subramani <167963812+Giriprasanth-Subramani@users.noreply.github.com>
2024-07-05 21:00:04 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -11,14 +11,7 @@ def get_soundex_code(c):
     return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
 
-def generate_soundex(name):
-    if not name:
-        return ""
-
-    # Start with the first letter (capitalized)
-    soundex = name[0].upper()
-    prev_code = get_soundex_code(soundex)
-
+def char_mapping(soundex, name, prev_code):
     for char in name[1:]:
         code = get_soundex_code(char)
         if code != '0' and code != prev_code:
@@ -26,7 +19,17 @@ def generate_soundex(name):
             prev_code = code
         if len(soundex) == 4:
             break
+    return soundex
 
+def generate_soundex(name):
+    if not name:
+        return ""
+
+    # Start with the first letter (capitalized)
+    soundex = name[0].upper()
+    prev_code = get_soundex_code(soundex)
+    soundex = char_mapping(soundex, name, prev_code)
+ 
     # Pad with zeros if necessary
     soundex = soundex.ljust(4, '0')
 
