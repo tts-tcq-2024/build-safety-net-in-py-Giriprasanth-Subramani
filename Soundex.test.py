7723b3e96ad266847d1569e3982eaf9f80d6914c
Giriprasanth-Subramani <167963812+Giriprasanth-Subramani@users.noreply.github.com>
2024-07-07 12:26:22 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -5,8 +5,14 @@ class TestSoundex(unittest.TestCase):
     def test_cases(self):
         self.assertEqual(generate_soundex(""), "")
         self.assertEqual(generate_soundex("G"), "G000")
+        self.assertEqual(generate_soundex("Saravanan"), "S615")
+        self.assertEqual(generate_soundex("Rupert"), "R163")
+        self.assertEqual(generate_soundex("Air"), "A600")
         self.assertEqual(generate_soundex("Ashcraft"), "A261")
+        self.assertEqual(generate_soundex("Ashcroft"), "A261")
         self.assertEqual(generate_soundex("Pfister"), "P236")
+        self.assertEqual(generate_soundex("A23F"), "A100")
+        self.assertEqual(generate_soundex("AEIOU"), "A000")
 
 
 if __name__ == '__main__':
